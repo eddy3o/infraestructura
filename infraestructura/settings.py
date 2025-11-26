bab8519e446590e0bd92f27108073be710d9bff3
eddy3o <edgardo.gonzalez.23@hotmail.com>
2025-11-26 01:02:17 -0600

    allowed hosts

--- infraestructura/settings.py
+++ infraestructura/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['13.221.212.225', '127.0.0.1',]
+ALLOWED_HOSTS = ['13.221.212.225', '127.0.0.1', 'https://d2dwrpm5ogrphi.cloudfront.net/']
 
 
 # Application definition
