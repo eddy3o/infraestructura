4cb9eaa7cb23ae737fb8c7087456cc7c362231cf
eddy3o <edgardo.gonzalez.23@hotmail.com>
2025-11-25 22:37:31 -0600

    first commit

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    cv = models.FileField(upload_to='cvs/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
